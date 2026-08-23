8f4782bb253917bf0e1fd0aab9915c059b313606
Peesala Naveen <naveenpeesala2004@gmail.com>
2026-08-23 14:00:32 +0530

    Add files via upload

---
+++ Ashley_coding_round.py
@@ -0,0 +1,44 @@
+password = input()
+score = 0
+failed=[]
+
+if len(password)>=8:
+    score+=1
+else:
+    failed.append("length")
+if any(ch.isupper() for ch in password):
+    score+=1
+else:
+    failed.append("uppercase")
+if any(ch.islower() for ch in password):
+    score+=1
+else:
+    failed.append("lowercase")
+if any(ch.isdigit() for ch in password):
+    score+=1
+else:
+    failed.append("digit")
+
+special="!@#$%^&*()_+-=[]{};:'",.<>|~"
+
+if any(ch in special for ch in password):
+    score+=1
+else:
+    failed.append("special")
+if " " not in password:
+    score+=1
+else:
+    failed.append("Space")
+if score==6:
+    print("Valid score:6")
+else:
+    print("InValid :" + ",".join(failed) + "score:" + str(score)) 
+
+
+
+
+
+
+
+    
+
